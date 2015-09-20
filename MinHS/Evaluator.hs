8e9e17fc084a8737a5738eee37e1af0156f792ed
zhangruoxu <yzhang@cse.unsw.edu.au>
2015-09-20 14:09:33 +1000

    Add function abstraction. Add checking scripts and test cases.

--- MinHS/Evaluator.hs
+++ MinHS/Evaluator.hs
@@ -42,10 +42,11 @@ evalE env (Var id) = case E.lookup env id of
 
 -- let expression
 evalE env (Let [] e) = evalE env e
-evalE env (Let ((Bind id t [] e1) : xs) e2) = evalE (E.add env (id, (evalE env e1))) (Let xs e2)
+evalE env (Let ((Bind id _ [] e1) : xs) e2) = evalE (E.add env (id, (evalE env e1))) (Let xs e2)
+evalE env (Let (f@(Bind func _ _ _) : xs) e2) = evalE (E.add env (func, (Func f))) (Let xs e2)
 
 -- primitive unary operation
-evalE env (App (Prim Ne) e) = let I i = evalE env e in I $ negate $ i
+evalE env (App (Prim Neg) e) = let I i = evalE env e in I $ negate $ i
 
 -- primitive binary operations
 evalE env (App (App (Prim op) e1) e2) = 
@@ -66,6 +67,7 @@ evalE env (App (App (Prim op) e1) e2) =
         Lt    -> B $ i1 < i2
         Le    -> B $ i1 <= i2
         Eq    -> B $ i1 == i2
+        Ne    -> B $ i1 /= i2
 
 -- if statement
 evalE env (If guard e1 e2) = 
@@ -88,6 +90,9 @@ evalE env (App (Prim Null) list) = case evalE env list of
 
 evalE env (App (App (Con "Cons") e1) e2) = let I i = evalE env e1 in Cons i (evalE env e2)
 
+-- function abstraction
+evalE env (Letfun bind) = Func bind
+
 -- unary function application
 evalE env (App (Letfun (Bind func _ [] body)) e)       = evalE (E.add env (func, (evalE env body))) (App (Var func) e)
 evalE env (App (Letfun f@(Bind func _ [para] body)) e) = evalE (E.add (E.add env (para, evalE env e)) (func, Func f)) body
@@ -97,21 +102,26 @@ evalE env (App (Prim op) e) = PartialPrim op (evalE env e)
 evalE env (App (Con "Cons") e) = PartialCon (evalE env e)
 
 -- partial function application
-evalE env (App (Var func) e) = case evalE env e of
-    (I i2) -> case evalE env (Var func) of
-        PartialPrim Add (I i1) -> I $ i1 + i2
-        PartialPrim Sub (I i1) -> I $ i1 - i2
-        PartialPrim Mul (I i1) -> I $ i1 * i2
-        PartialPrim Quot (I i1) -> case i2 of
-            0 -> error "Exception: divide by zero"
-            _ -> I $ quot i1 i2
-        PartialPrim Rem (I i1) -> case i2 of
-            0 -> error "Exception: divide by zero"
-            _ -> I $ rem i1 i2
-    (B b2) -> case evalE env (Var func) of 
-        PartialPrim Gt (B b1) -> B $ b1 > b2
-        PartialPrim Ge (B b1) -> B $ b1 >= b2
-        PartialPrim Lt (B b1) -> B $ b1 < b2
-        PartialPrim Le (B b1) -> B $ b1 <= b2
-        PartialPrim Eq (B b1) -> B $ b1 == b2
-   
+evalE env (App v@(Var func) e) = case evalE env v of
+    Func f                  -> evalE env (App (Letfun f) e)
+    PartialPrim Add (I i1)  -> I $ i1 + i2
+    PartialPrim Sub (I i1)  -> I $ i1 - i2
+    PartialPrim Mul (I i1)  -> I $ i1 * i2
+    PartialPrim Quot (I i1) -> case i2 of
+        0 -> error "Exception: divide by zero"
+        _ -> I $ quot i1 i2
+    PartialPrim Rem (I i1)  -> case i2 of 
+        0 -> error "Exception: divide by zero"
+        _ -> I $ rem i1 i2
+    PartialCon (I i)        -> Cons i (evalE env e)
+    PartialPrim Gt (B b1)   -> B $ b1 > b2
+    PartialPrim Ge (B b1)   -> B $ b1 >= b2
+    PartialPrim Lt (B b1)   -> B $ b1 < b2
+    PartialPrim Le (B b1)   -> B $ b1 <= b2
+    PartialPrim Eq (B b1)   -> B $ b1 == b2
+    PartialPrim Ne (B b1)   -> B $ b1 /= b2
+    where I i2 = evalE env e 
+          B b2 = evalE env e
+
+-- evalE _ e = error $ show e
+
